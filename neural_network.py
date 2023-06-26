e6fb026895126525260c1d8efde42cfbbf54ddd7
jhoanmartinez <jhoao1320@gmail.com>
2023-06-26 11:03:23 -0500

    Add ReLU class

--- neural_network.py
+++ neural_network.py
@@ -2,11 +2,15 @@ import numpy as np
 
 class LayerDense:
 
-    def __init__(self, inputs, neuron) -> None:
-        self.weights = 0.01 * np.random.rand(inputs, neuron)
-        self.bias = np.zeros( (1, neuron) )
+    def __init__(self, inputs_size, neuron_size):
+        self.weights = 0.01 * np.random.rand(inputs_size, neuron_size)
+        self.bias = np.zeros( (1, neuron_size) )
 
-    def forward(self, input):
-        self.output = np.dot(input, self.weights) + self.bias
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.bias
 
+class  ReLU:
+
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
         
