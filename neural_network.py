e2e5e1de6032fac9787afcadbd3149087ab049da
jhoanmartinez <jhoao1320@gmail.com>
2023-06-26 19:51:21 -0500

    Add fixed weights to do math

--- neural_network.py
+++ neural_network.py
@@ -3,14 +3,30 @@ import numpy as np
 class LayerDense:
 
     def __init__(self, inputs_size, neuron_size):
-        self.weights = 0.01 * np.random.rand(inputs_size, neuron_size)
+        # self.weights = 0.01 * np.random.rand(inputs_size, neuron_size)
+        self.weights = [
+                        [1.1, 1.2, 1.3],
+                        [1.2, 1.2, 1.3]
+                    ]
         self.bias = np.zeros( (1, neuron_size) )
 
-    def forward(self, inputs):
+    def forward_layer(self, inputs):
+        inputs = [
+        [1, 2],
+        [7, 8],
+        [2, 9]
+    ]
         self.output = np.dot(inputs, self.weights) + self.bias
 
 class  ReLU:
 
+    def forward_relu(self, inputs):
+        self.output = np.maximum(10, inputs)
+
+class Softmax:
+
     def forward(self, inputs):
-        self.output = np.maximum(0, inputs)
+        pass
+
+
         
