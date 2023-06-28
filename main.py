293aa2c518e40a8bbbf753e4cf27dacc7fe89377
jhoanmartinez <jhoao1320@gmail.com>
2023-06-28 09:20:35 -0500

    architecture and forward in all layers

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import numpy as np
-from neural_network import LayerDense, ReLU
-from dataset import generate_dataset, experiment_dataset
+from neural_network import LayerDense, ReLU, Softmax
+from dataset import generate_dataset
 
-# Switch between dataset and a tiny dataset to check process and math
-experiment = 1
 
-if experiment == 1:
-    # Tiny dataset to validate formulas and process
-    X_data, y_category = experiment_dataset()
-else:
-    # Large dataset generate_data(samples_in_2dimension, number_of_Categories)
-    X_data, y_category = generate_dataset(70, 3)
+# Large dataset generate_data(samples_in_2dimension, number_of_Categories)
+X_data, y_category = generate_dataset(3, 3)
+
+""" ======>>>>>>>   Layers architecture <<<<<<======"""
 
 # Layer dense with two inputs and 3 output values
 layer1 = LayerDense(2, 3)
@@ -18,11 +14,20 @@ layer1 = LayerDense(2, 3)
 # ReLU layer
 relu1 = ReLU()
 
+# Softmax 1
+softmax1 = Softmax()
+
 # Create forward pass
 layer1.forward_layer(X_data)
+print("forward pass =\n",layer1.output)
 
 # ReLU activation
 relu1.forward_relu(layer1.output)
+print("\nrelu activation =\n", relu1.output)
+
+# Softmax activation
+softmax1.forward_softmax(relu1.output)
+print("\nSoftmax activation =\n", softmax1.output)
 
 
 
