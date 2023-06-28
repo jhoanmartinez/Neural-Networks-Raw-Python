d18f373497266042910ca9bbb739f7a41fec3f43
jhoanmartinez <jhoao1320@gmail.com>
2023-06-28 09:19:45 -0500

    Add softmax to avoid exploding or dead neurons

--- neural_network.py
+++ neural_network.py
@@ -3,30 +3,23 @@ import numpy as np
 class LayerDense:
 
     def __init__(self, inputs_size, neuron_size):
-        # self.weights = 0.01 * np.random.rand(inputs_size, neuron_size)
-        self.weights = [
-                        [1.1, 1.2, 1.3],
-                        [1.2, 1.2, 1.3]
-                    ]
+        self.weights = 0.01 * np.random.rand(inputs_size, neuron_size)
         self.bias = np.zeros( (1, neuron_size) )
 
     def forward_layer(self, inputs):
-        inputs = [
-        [1, 2],
-        [7, 8],
-        [2, 9]
-    ]
         self.output = np.dot(inputs, self.weights) + self.bias
 
 class  ReLU:
 
     def forward_relu(self, inputs):
-        self.output = np.maximum(10, inputs)
+        self.output = np.maximum(0, inputs)
 
 class Softmax:
 
-    def forward(self, inputs):
-        pass
+    def forward_softmax(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        distribution_values = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = distribution_values
 
 
         
