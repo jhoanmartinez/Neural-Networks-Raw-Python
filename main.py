a85bf870e558bd40563cf64ca5927385614b46ca
jhoanmartinez <jhoao1320@gmail.com>
2023-06-29 12:06:28 -0500

    Adjust code like pytorch calling

--- main.py
+++ main.py
@@ -6,32 +6,16 @@ from dataset import generate_dataset
 # Large dataset generate_data(samples_in_2dimension, number_of_Categories)
 X_data, y_category = generate_dataset(3, 3)
 
-""" ======>>>>>>>   Layers architecture <<<<<<======"""
+layer1 = LayerDense(2, 3) # Layer dense with two inputs and 3 output values
+relu1 = ReLU()            # ReLU layer
+softmax1 = Softmax()      # Softmax 1
 
-# Layer dense with two inputs and 3 output values
-layer1 = LayerDense(2, 3)
+layer1.forward_layer(X_data)           # Create forward pass
+relu1.forward_relu(layer1.output)      # ReLU activation
+softmax1.forward_softmax(relu1.output) # Softmax activation
 
-# ReLU layer
-relu1 = ReLU()
 
-# Softmax 1
-softmax1 = Softmax()
-
-# Create forward pass
-layer1.forward_layer(X_data)
 print("forward pass =\n",layer1.output)
-
-# ReLU activation
-relu1.forward_relu(layer1.output)
 print("\nrelu activation =\n", relu1.output)
-
-# Softmax activation
-softmax1.forward_softmax(relu1.output)
 print("\nSoftmax activation =\n", softmax1.output)
-
-
-
-
-
-
 
