6528fe48509ab020a3c8e1f7626cd924260fe2b1
jhoanmartinez <jhoao1320@gmail.com>
2023-07-02 09:12:42 -0500

    BCEL layer

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import numpy as np
 from neural_network import LayerDense, ReLU, Softmax, BinaryCrossEntropyLoss
 from dataset import generate_dataset
 
@@ -6,15 +5,15 @@ from dataset import generate_dataset
 # Large dataset generate_data(samples_in_2dimension, number_of_Categories)
 X_data, y_category = generate_dataset(3, 3)
 
-layer1 = LayerDense(2, 3) # Layer dense with two inputs and 3 output values
-relu1 = ReLU()            # ReLU layer
-softmax1 = Softmax()      # Softmax 1
-bce_loss = BinaryCrossEntropyLoss()
+layer1 = LayerDense(2, 3)           # Layer dense with two inputs and 3 output values
+relu1 = ReLU()                      # ReLU layer
+softmax1 = Softmax()                # Softmax layer
+bce_loss = BinaryCrossEntropyLoss() # Binary cross entropy loss layer
 
-layer1.forward_layer(X_data)           # Create forward pass
-relu1.forward_relu(layer1.output)      # ReLU activation
-softmax1.forward_softmax(relu1.output) # Softmax activation
-bce_loss.forward_BCEL(softmax1.output, y_category)
+layer1.forward_layer(X_data)                        # Create forward pass
+relu1.forward_relu(layer1.output)                   # ReLU activation
+softmax1.forward_softmax(relu1.output)              # Softmax activation
+bce_loss.forward_BCEL(softmax1.output, y_category)  # Binary cross entroypy loss
 
 print("forward pass =\n",layer1.output)
 print("\nrelu activation =\n", relu1.output)
