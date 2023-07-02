d17bd99c6001ec6ef06f8ddfbc3caac100e3cf49
jhoanmartinez <jhoao1320@gmail.com>
2023-07-02 10:23:15 -0500

    Accuracy layer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from neural_network import LayerDense, ReLU, Softmax, BinaryCrossEntropyLoss
+from neural_network import LayerDense, ReLU, Softmax, BinaryCrossEntropyLoss, Accuracy
 from dataset import generate_dataset
 
 
@@ -9,13 +9,16 @@ layer1 = LayerDense(2, 3)           # Layer dense with two inputs and 3 output v
 relu1 = ReLU()                      # ReLU layer
 softmax1 = Softmax()                # Softmax layer
 bce_loss = BinaryCrossEntropyLoss() # Binary cross entropy loss layer
+accuracy = Accuracy()
 
 layer1.forward_layer(X_data)                        # Create forward pass
 relu1.forward_relu(layer1.output)                   # ReLU activation
 softmax1.forward_softmax(relu1.output)              # Softmax activation
 bce_loss.forward_BCEL(softmax1.output, y_category)  # Binary cross entroypy loss
+accuracy.forward_accuracy(softmax1.output, y_category) # Accuracy total
 
 print("forward pass =\n",layer1.output)
 print("\nrelu activation =\n", relu1.output)
 print("\nSoftmax activation =\n", softmax1.output)
 print("\nCross entropy loss =\n", bce_loss.output)
+print("\nAccuracy total =\n", accuracy.output)
